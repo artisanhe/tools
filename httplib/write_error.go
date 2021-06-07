19a8fef2eb04e138f23dae74e0b0c11656b6da91
hejiacheng <hejiacheng@g7.com.cn>
2021-06-07 10:29:58 +0800

    1.add code;

--- httplib/write_error.go
+++ httplib/write_error.go
@@ -1,6 +1,7 @@
 package httplib
 
 import (
+    "net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -27,11 +28,32 @@ func errorWithSource(err *status_error.StatusError) *status_error.StatusError {
 }
 
 func WriteError(c *gin.Context, err error) {
+	wrapErrorForEmqx(c, err)
+	//statusError := status_error.FromError(err)
+	//if statusError.Code == int64(status_error.UnknownError) {
+	//	logrus.Warnf("got UnknownError %s", err.Error())
+	//}
+	//statusError = errorWithSource(statusError)
+	//c.Error(statusError)
+	//c.JSON(statusError.Status(), statusError)
+}
+
+type EmqxError struct {
+	Code    int32  `json:"code"`
+	Message string `json:"message"`
+	Data struct{} `json:"data"`
+}
+
+func wrapErrorForEmqx(c *gin.Context, err error) {
 	statusError := status_error.FromError(err)
 	if statusError.Code == int64(status_error.UnknownError) {
 		logrus.Warnf("got UnknownError %s", err.Error())
 	}
-	statusError = errorWithSource(statusError)
-	c.Error(statusError)
-	c.JSON(statusError.Status(), statusError)
+
+	errResp := EmqxError{
+		Code : int32(statusError.Code),
+		Message: statusError.Desc + statusError.Msg + statusError.ErrorFields.String(),
+	}
+
+	c.JSON(http.StatusOK, errResp)
 }
